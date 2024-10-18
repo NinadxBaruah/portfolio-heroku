6f9ccd15602cbee413559e2766b58cedb6c721de
NinadxBaruah <ninadbaruah882@gmail.com>
2024-10-18 21:25:57 +0530

    all content in client deleted except dist

--- client/src/main.tsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import {appRouter} from './App.tsx'
-import { RouterProvider } from 'react-router-dom'
-import './index.css'
-
-createRoot(document.getElementById('root')!).render(
-    <RouterProvider router={appRouter}/>
-)
