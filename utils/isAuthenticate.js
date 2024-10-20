9f0689db9cd1dd0c1a1a2ea30cbfb65fc223f57e
NinadxBaruah <ninadbaruah882@gmail.com>
2024-10-20 12:35:48 +0530

    db logs are removed from the isAuth and authToken.js is removed as we do not nedd it anymore

--- utils/isAuthenticate.js
+++ utils/isAuthenticate.js
@@ -1,10 +1,8 @@
 const jwt = require("jsonwebtoken");
-const tokenString = require("../db/authToken")
 
 const isAuthenticate = async (req, res, next) => {
   const authToken = req.cookies.__authToken;
 
-  const createToken = await tokenString.create({token:authToken})
 
   if (!authToken) {
     return res.status(401).json({ message: "Unauthorized" });
@@ -14,19 +12,15 @@ const isAuthenticate = async (req, res, next) => {
     if (err) {
       if (err.name === "TokenExpiredError") {
         console.log("error: Token has expired");
-        const authLog = await tokenString.create({logs:err})
         return res.status(401).json({ message: "Token expired" });
       } else if (err.name === "JsonWebTokenError") {
         console.log("error: Invalid token");
-        const authLog = await tokenString.create({logs:err})
         return res.status(403).json({ message: "Invalid token" });
       } else if (err.name === "NotBeforeError") {
         console.log("error: Token not active");
-        const authLog = await tokenString.create({logs:err})
         return res.status(403).json({ message: "Token not yet active" });
       } else {
         console.log("error:", err);
-        const authLog = await tokenString.create({logs:err})
         return res.status(403).json({ message: "Forbidden", error:err });
       }
     }
