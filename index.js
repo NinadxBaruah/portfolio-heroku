918994292511fe8df747e1b0cd9e256809955451
NinadxBaruah <ninadbaruah882@gmail.com>
2024-10-22 00:26:53 +0530

    add not found page

--- index.js
+++ index.js
@@ -66,6 +66,10 @@ app.use(express.static(path.resolve(__dirname, "public")));
 app.use("/projects", projects);
 app.use("/", homepage);
 
+// 404 Page Not Found route
+app.use((req, res, next) => {
+  res.status(404).render("not-found", { title: "Page Not Found" });
+});
 // Start server
 const server = app.listen(PORT, () => {
   console.log(`Server listening in the port: ${PORT}`);
