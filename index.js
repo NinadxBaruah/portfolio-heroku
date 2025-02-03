1b89e9f6d3a8b099d3fcbe31e7c12782eefbb03c
NinadxBaruah <ninadbaruah882@gmail.com>
2025-02-03 13:08:18 +0530

    index file updated

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const path = require("path");
-<<<<<<< HEAD
 const bodyParser = require('body-parser')
-=======
->>>>>>> d7ed9e52fc5ee44a4aefece0e255f321b89cf92d
+
 const fs = require("fs");
 require("dotenv").config();
 require("./db/db");
@@ -11,22 +9,16 @@ const cors = require("cors");
 const configureWebSocket = require("./socket/index");
 const cookieParser = require("cookie-parser");
 const fileUpload = require("express-fileupload");
-<<<<<<< HEAD
 // const connectDB2 = require("./db/db2");
-=======
 
->>>>>>> d7ed9e52fc5ee44a4aefece0e255f321b89cf92d
 const projects = require("./routes/http.route/projects");
 const homepage = require("./routes/http.route/homepage");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const app = express();
-<<<<<<< HEAD
 app.use(bodyParser.urlencoded({ extended: false }))
 // connectDB2();
-=======
 
->>>>>>> d7ed9e52fc5ee44a4aefece0e255f321b89cf92d
 // Trust the first proxy
 app.set("trust proxy", 1);
 
@@ -50,7 +42,6 @@ app.use(
 // }
 
 // Configure express-fileupload with memory efficient options
-<<<<<<< HEAD
 // app.use(
 //   fileUpload({
 //     useTempFiles: true,
@@ -65,20 +56,7 @@ app.use(
 // );
 
 app.use('/uploads', express.static(path.join(process.cwd(), 'public', 'uploads')));
-=======
-app.use(
-  fileUpload({
-    useTempFiles: true,
-    tempFileDir: path.join(__dirname, "uploads", "temp"),
-    limits: {
-      fileSize: 5 * 1024 * 1024, // 5MB limit
-      files: 1, // Allow only 1 file upload at a time
-    },
-    abortOnLimit: true,
-    debug: false,
-  })
-);
->>>>>>> d7ed9e52fc5ee44a4aefece0e255f321b89cf92d
+
 
 // Add these headers explicitly
 app.use((req, res, next) => {
@@ -100,12 +78,9 @@ app.set("views", path.join(__dirname, "views"));
 if (process.env.NODE_ENV === "production") {
   app.use(
     "/projects/chat-app",
-<<<<<<< HEAD
     express.static(path.join(__dirname, "dist"),
      {
-=======
-    express.static(path.join(__dirname, "dist"), {
->>>>>>> d7ed9e52fc5ee44a4aefece0e255f321b89cf92d
+
       setHeaders: (res, filepath) => {
         if (filepath.endsWith(".js")) {
           res.set("Content-Type", "application/javascript");
@@ -117,26 +92,23 @@ if (process.env.NODE_ENV === "production") {
       },
     })
   );
-<<<<<<< HEAD
 
   app.use('/projects/react-intern', express.static(path.join(__dirname, 'react-intern-build')));
   app.use('/projects/intern/3', express.static(path.join(__dirname, 'react-intern-build2')));
   app.use('/projects/intern/4', express.static(path.join(__dirname, 'react-intern-build3')));
+  // app.use('/projects/intern/5', express.static(path.join(__dirname, 'react-intern-build4')));
 
-=======
->>>>>>> d7ed9e52fc5ee44a4aefece0e255f321b89cf92d
+
 }
 
 // Other static files
 app.use(express.static(path.resolve(__dirname, "public")));
 
 // Routes
-<<<<<<< HEAD
 app.get('/resume',(req,res) =>{
   res.render('resume')
 })
-=======
->>>>>>> d7ed9e52fc5ee44a4aefece0e255f321b89cf92d
+
 app.use("/projects", projects);
 app.use("/", homepage);
 
